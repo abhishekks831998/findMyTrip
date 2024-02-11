7bd57b012613600f24a913bdcbb2ec4380e24cd1
Jay Kareliya <j_kareli@live.concordia.ca>
2024-02-11 14:53:50 -0500

    v1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [RouterOutlet, FormsModule],
+  template: `
+  <input [(ngModel)]="name" type="text">
+  {{name}}
+  `,
+  styles: []
 })
 export class AppComponent {
-  title = 'helloword';
+  public name : string = '';
+
+  logMessage(value: string){
+    console.log(value);
+  }
 }
