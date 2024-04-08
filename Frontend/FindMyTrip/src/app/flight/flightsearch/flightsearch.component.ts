d327f79ad8c5a6d337117f29be0c5d3c294f5097
bhargavraval12 <b_raval@live.concordia.ca>
2024-04-07 22:19:08 -0400

    updated flight search code
    searching just with typing

--- Frontend/FindMyTrip/src/app/flight/flightsearch/flightsearch.component.ts
+++ Frontend/FindMyTrip/src/app/flight/flightsearch/flightsearch.component.ts
@@ -1,30 +1,44 @@
-import {Component, EventEmitter, Output} from '@angular/core';
-import {SharedService} from "../../shared.service";
+import { Component, EventEmitter, Output } from '@angular/core';
+import { SharedService } from "../../shared.service";
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'app-flightsearch',
   templateUrl: './flightsearch.component.html',
-  styleUrl: './flightsearch.component.css'
+  styleUrls: ['./flightsearch.component.css']
 })
 export class FlightsearchComponent {
 
-@Output() changeflightList = new EventEmitter<number>();
+  @Output() changeflightList = new EventEmitter<number>();
   query: string = '';
   searchResults: any[] = [];
-  constructor(private flightSearchService: SharedService) {}
+  private searchQueryChanged = new Subject<string>();
+
+  constructor(private flightSearchService: SharedService) {
+    this.searchQueryChanged.pipe(
+      debounceTime(300)
+    ).subscribe(() => {
+      this.search();
+    });
+  }
+
+  onQueryChange(): void {
+        this.searchQueryChanged.next(this.query); // Emit event to trigger search
+  }
 
   search(): void {
     if (!this.query.trim()) {
       this.flightSearchService.getFlightList().subscribe(
-      (data) => {
-        let flights = JSON.stringify(data);
-        let flightList = JSON.parse(flights);
-        this.changeflightList.emit(flightList.results);
-      },
-      (error) => {
-        console.error(error);
-      }
-    );
+        (data) => {
+          let flights = JSON.stringify(data);
+          let flightList = JSON.parse(flights);
+          this.changeflightList.emit(flightList.results);
+        },
+        (error) => {
+          console.error(error);
+        }
+      );
     } // Ignore empty queries
 
     this.flightSearchService.searchFlights(this.query).subscribe(
