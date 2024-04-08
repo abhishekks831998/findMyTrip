9a5b14525215fecd0b9a280caf4e3d90b0f6a9c4
bhargavraval12 <b_raval@live.concordia.ca>
2024-04-07 22:50:33 -0400

    updated search functionality in hotel

--- Frontend/FindMyTrip/src/app/hotel/hotelsearch/hotelsearch.component.ts
+++ Frontend/FindMyTrip/src/app/hotel/hotelsearch/hotelsearch.component.ts
@@ -1,30 +1,44 @@
-import {Component, EventEmitter, Output} from '@angular/core';
-import {SharedService} from "../../shared.service";
+import { Component, EventEmitter, Output } from '@angular/core';
+import { SharedService } from "../../shared.service";
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'app-hotelsearch',
   templateUrl: './hotelsearch.component.html',
-  styleUrl: './hotelsearch.component.css'
+  styleUrls: ['./hotelsearch.component.css']
 })
 export class HotelsearchComponent {
 
-@Output() changehotelList = new EventEmitter<number>();
+  @Output() changehotelList = new EventEmitter<number>();
   query: string = '';
   searchResults: any[] = [];
-  constructor(private hotelSearchService: SharedService) {}
+  private searchQueryChanged = new Subject<string>();
+
+  constructor(private hotelSearchService: SharedService) {
+    this.searchQueryChanged.pipe(
+      debounceTime(300) // Adjust debounce time as needed (300ms in this case)
+    ).subscribe(query => {
+      this.search();
+    });
+  }
+
+  onQueryChange(): void {
+    this.searchQueryChanged.next(this.query);
+  }
 
   search(): void {
     if (!this.query.trim()) {
       this.hotelSearchService.getHotelList().subscribe(
-      (data) => {
-        let hotels = JSON.stringify(data);
-        let hotelList = JSON.parse(hotels);
-        this.changehotelList.emit(hotelList.results);
-      },
-      (error) => {
-        console.error(error);
-      }
-    );
+        (data) => {
+          let hotels = JSON.stringify(data);
+          let hotelList = JSON.parse(hotels);
+          this.changehotelList.emit(hotelList.results);
+        },
+        (error) => {
+          console.error(error);
+        }
+      );
     } // Ignore empty queries
 
     this.hotelSearchService.searchHotels(this.query).subscribe(
@@ -38,4 +52,3 @@ export class HotelsearchComponent {
     );
   }
 }
-
