b0a5917349040863458f6a265cfb5f1260c7fa33
bhargavraval12 <b_raval@live.concordia.ca>
2024-04-13 23:30:08 -0400

    Search bug fix for show packges

--- Frontend/FindMyTrip/src/app/package/show-packages/show-packages.component.ts
+++ Frontend/FindMyTrip/src/app/package/show-packages/show-packages.component.ts
@@ -1,42 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-import {PackageService} from "../package.service";
-import {Router} from "@angular/router";
-
-// This component is responsible for showing all the packages available in the system.
-
-interface Package {
-  id: number;
-  name: string;
-  description: string;
-  duration_in_days: number;
-  hotels: any;
-  flights: any;
-  activities: any;
-  image: string;
-  created_by: number;
-}
+import { PackageService } from "../package.service";
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-show-packages',
   templateUrl: './show-packages.component.html',
-  styleUrl: './show-packages.component.css'
+  styleUrls: ['./show-packages.component.css']
 })
 export class ShowPackagesComponent implements OnInit {
 
-  constructor(private service: PackageService, private router: Router) {
-  }
+  constructor(private service: PackageService, private router: Router) { }
 
   ActivateAddEditPackageComp: boolean = false;
+  OriginalPackageList: any = []; // Variable to store the original list of packages
   PackageList: any = [];
-  GeneralPackageList: Package[] = [];
   package: any;
   ModelTitle: string | undefined;
-  customPackages: Package[] = [];
-  userID = 0;
-
+  searchQuery: string = '';
 
   ngOnInit(): void {
-    this.refreshPackageList()
+    this.refreshPackageList();
   }
 
   addClick(): void {
@@ -48,21 +31,20 @@ export class ShowPackagesComponent implements OnInit {
       hotels: {},
       flights: {},
       activities: {},
-      image: "",
+      image: ""
     }
     this.ModelTitle = "Add Package";
     this.ActivateAddEditPackageComp = true;
   }
 
   editPackage(data: any): void {
-    console.log("edit clicked");
     this.package = data;
     this.ActivateAddEditPackageComp = true;
     this.ModelTitle = "Edit Package";
   }
 
   deletePackage(packageId: number): void {
-    if (confirm('Are you sure??')) {
+    if (confirm('Are you sure?')) {
       this.service.deletePackage(packageId).subscribe(data => {
         alert(data.toString());
         this.refreshPackageList();
@@ -70,37 +52,31 @@ export class ShowPackagesComponent implements OnInit {
     }
   }
 
-  closeClick(): void {
-    this.ActivateAddEditPackageComp = false;
-    this.refreshPackageList();
+  viewPackageDetails(packageId: string, packageObj: any): void {
+    this.router.navigate(['/package-details', packageId], { state: { data: packageObj } });
   }
 
-  filterPackges(): void {
-    const userId  = parseInt(localStorage.getItem('userId') || '0', 10);
-    console.log(userId);
-    if (userId !== 0) {
-      for (let i = 0; i < this.PackageList.length; i++) {
-        console.log(this.PackageList[i].created_by);
-        if (this.PackageList[i].created_by === userId) {
-          this.customPackages.push(this.PackageList[i]);
-        }
-      }
+  onSearch(): void {
+    if (this.searchQuery.trim() === '') {
+      this.PackageList = [...this.OriginalPackageList]; // Reset PackageList to original list when search query is empty
+    } else {
+      this.PackageList = this.OriginalPackageList.filter((pkg: any) => pkg.name.toLowerCase().includes(this.searchQuery.toLowerCase()));
     }
-    this.GeneralPackageList = this.PackageList.filter((p: { created_by: number | 0; }) => p.created_by === 0);
-    console.log(this.customPackages);
   }
-
- viewPackageDetails(packageId: number,packageObj:any): void {
-  this.router.navigate(['/package-details', packageId], {state: {data: packageObj}});
-}
 
   refreshPackageList() {
     this.service.getPackageList().subscribe(data => {
       let packages = JSON.stringify(data);
       let packageList = JSON.parse(packages);
-      this.PackageList = packageList.results;
-      this.filterPackges();
+      this.OriginalPackageList = packageList.results; // Store original list of packages
+      this.PackageList = [...this.OriginalPackageList]; // Copy original list to PackageList
     });
   }
+
+  closeClick(): void {
+    this.ActivateAddEditPackageComp = false;
+  }
 }
+
+
 
